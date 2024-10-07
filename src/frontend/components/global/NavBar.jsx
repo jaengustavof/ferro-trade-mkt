f0a9cfbae63f70e068d30bcd14ead5969589e516
netzulo <netzuleando@gmail.com>
2024-10-07 03:18:32 +0200

    [josue] add blockauth auth system

--- src/frontend/components/global/NavBar.jsx
+++ src/frontend/components/global/NavBar.jsx
@@ -1,9 +1,13 @@
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import ConnectWallet from "../ad-hoc/menu/ConnectWallet";
 import { TiThMenu } from "react-icons/ti";
 import { IoCloseCircle } from "react-icons/io5";
 import { CartMenu } from "../ad-hoc/menu/CartMenu";
 import useIsOwner from "../../hooks/useIsOwner";
+import { DropdownProfile, FlowBlockAuth } from 'blockauth-sdk-react';
+// blockauth-sdk-react CSS
+import 'blockauth-sdk-react/public/output.css'
 
 export const NavBar = () => {
 
@@ -23,6 +27,19 @@ export const NavBar = () => {
   const handleMenuClick = () => {
     document.getElementById("menuContainer").classList.toggle("show");
   }
+  // TODO: delete, just for dev
+  const [isLogged, setIsLogged] = useState(false)
+
+  const onSuccess = (address) => {
+    setIsLogged(true)
+    // TODO: call to normal login
+    // handleLogin({
+    //     email: getValues("email"),
+    //     password: getValues("password"),
+    //     address: address,
+    // })
+  }
+
   return (
     <div className="nav-conatainer">
       <nav>
@@ -57,7 +74,26 @@ export const NavBar = () => {
         </div>
         <div className="actions-container">
           <CartMenu />
-          <ConnectWallet />
+          {/* <ConnectWallet /> */}
+          <div>
+              {(isLogged) && <>
+              <DropdownProfile
+                  onLogout={()=>{
+                      setIsLogged(false);
+                  }}
+                  onSuccess={onSuccess}
+                  apiKey={"app.2927cdaa-3a27-5e98-a685-a03cfb493b3f"}
+                  apiSecret={"5dd6f8243aa7778d541c833a80a9e7d7430068c60c0c9dc1b612ce8d91736612b21f96488e898e4dad0e11e173c46e830af5560a499fd34c9b05b96e17bed4dd.ffb02faacfedba7f"}
+              />
+              </>}
+              {(!isLogged) && <>
+              <FlowBlockAuth
+                  apiKey={"app.2927cdaa-3a27-5e98-a685-a03cfb493b3f"}
+                  apiSecret={"5dd6f8243aa7778d541c833a80a9e7d7430068c60c0c9dc1b612ce8d91736612b21f96488e898e4dad0e11e173c46e830af5560a499fd34c9b05b96e17bed4dd.ffb02faacfedba7f"}
+                  onSuccess={onSuccess}
+              />
+              </>}
+          </div>
         </div>
         
       </nav>
